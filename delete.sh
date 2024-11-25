76f2e54e9a47bfc47368aa5d5597b7fa8983a14b
JuizcyC <95832376+JuizcyC@users.noreply.github.com>
2024-11-25 13:20:34 -0500

    adds delete.sh

---
+++ delete.sh
@@ -0,0 +1,4 @@
+kubectl delete deployments --all --namespace=default
+kubectl delete services --all --namespace=default
+kubectl delete pvc --all --namespace=default
+kubectl delete configmaps --all --namespace=default
