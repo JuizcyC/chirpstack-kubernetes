5b2c71587cc4d989ba77dc295fba6c73a6446b01
JuizcyC <95832376+JuizcyC@users.noreply.github.com>
2024-11-27 13:59:02 -0500

    redeploy.sh

---
+++ redeploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+kubectl delete deployments --all --namespace=default
+kubectl delete services --all --namespace=default
+kubectl delete pvc --all --namespace=default
+kubectl delete configmaps --all --namespace=default
+kubectl delete pv postresqldata
+git pull
+kubectl apply -f config-maps
+kubectl apply -f volumes
+kubectl apply -f services
+kubectl apply -f deployments
