e276f1010a823ccee1a17932161198d8759ef7c5
JuizcyC <95832376+JuizcyC@users.noreply.github.com>
2024-11-29 15:42:20 -0500

    And in redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -4,7 +4,7 @@ kubectl delete deployments --all --namespace=default
 kubectl delete services --all --namespace=default
 kubectl delete pvc --all --namespace=default
 kubectl delete configmaps --all --namespace=default
-kubectl delete pv us-chirpstack-postgres-data
+kubectl delete pv us-chirpstack-postgres
 git pull
 kubectl apply -f config-maps
 kubectl apply -f volumes
