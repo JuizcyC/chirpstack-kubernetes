60f357f7ba19606e90ade18aa76e348132ba2132
JuizcyC <95832376+JuizcyC@users.noreply.github.com>
2024-12-02 11:48:45 -0500

    updates redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -4,6 +4,8 @@ kubectl delete deployments --all --namespace=default
 kubectl delete services --all --namespace=default
 kubectl delete configmaps --all --namespace=default
 kubectl delete statefulset --all
+kubectl delete pvc --all --namespace=default
+kubectl delete pv --all
 git pull
 kubectl apply -f config-maps
 kubectl apply -f volumes
