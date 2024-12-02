e20af58b2b23846fc3eb4e3f4436450d789beed4
JuizcyC <95832376+JuizcyC@users.noreply.github.com>
2024-12-02 15:10:21 -0500

    Deleted certs setup chirpstack ingress

--- redeploy.sh
+++ redeploy.sh
@@ -6,8 +6,12 @@ kubectl delete configmaps --all --namespace=default
 kubectl delete statefulset --all
 kubectl delete pvc --all --namespace=default
 kubectl delete pv --all
+kubectl delete ingress-rules
+kubectl delete ingress-nginx
 git pull
 kubectl apply -f config-maps
 kubectl apply -f volumes
 kubectl apply -f services
 kubectl apply -f deployments
+kubectl apply -f ingress-nginx
+kubectl apply -f ingress-rules
